9328d38000ed551bae184336673822c43bc37a30
Adonai <a24110195@live.ceti.mx>
2025-10-09 02:02:10 +0000

    Practica 2.1

--- bin/Punteros.cpp
+++ bin/Punteros.cpp
@@ -14,5 +14,14 @@ int main() {
     std::cout << "direccion: " << pNum << '\n';
     std::cout << "valor: " << pNum << '\n';
 
+    cout << "Tipos de datos: " << endl;
+    cout << "Tamaño int : " << sizeof(int) << endl;
+    cout << "Tamaño long :" << sizeof(long) << endl;
+    cout << "Tamaño float :" << sizeof(float) << endl;
+    cout << "Tamaño double :" << sizeof(double) << endl;
+    cout << "Tamaño char :" << sizeof(char) << endl;
+    cout << "Tamaño bool :" << sizeof(bool) << endl;
+    cout << "Tamaño byte :" << sizeof(byte) << endl;
+
     return 0;
 }
